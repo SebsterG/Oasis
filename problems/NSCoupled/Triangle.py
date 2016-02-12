e13d8fdd56aa2cf48327ed09a0ebe96e1b8f55df
FEniCS <seb.gjertsen@gmail.com>
2016-02-12 10:20:44 +0100

    xml files

--- problems/NSCoupled/Triangle.py
+++ problems/NSCoupled/Triangle.py
@@ -6,7 +6,7 @@ mesh = Mesh("Triangle_corner_nice.xml")
 #plot(mesh) ;interactive()
 
 NS_parameters.update(
-	nu = 1.0/200.0,
+	nu = 1.0/100.0,
 	#use_krylov_solver = False,
     max_error = 1e-13,
     max_iter = 100,
